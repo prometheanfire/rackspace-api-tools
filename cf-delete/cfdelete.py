c73628458c229cf0c150a0ac78cc558817fc7120
Matthew Thode <mthode@mthode.org>
2012-12-20 15:18:24 -0600

    testing taging

--- cf-delete/cfdelete.py
+++ cf-delete/cfdelete.py
@@ -13,7 +13,7 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 """
-Rackspace Cloud Files Deletion Script
+Rackspace Cloud Files Deletion Script test git tags version
 """
 
 import argparse
